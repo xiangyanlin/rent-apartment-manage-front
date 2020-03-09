a0b27395979dceb95ec0014f8fa36a45aeb6be71
xiangyanlin <2356150341@qq.com>
2020-03-09 22:25:38 +0800

    Commit 房源列表数据 xyl

--- src/services/houseResource.js
+++ src/services/houseResource.js
@@ -2,5 +2,5 @@ import request from '@/utils/request';
 import { stringify } from 'qs';
 
 export async function queryResource(params) {
-  return request(`/xyl/resource?${stringify(params)}`);
+  return request(`/xyl/houseResources/list?${stringify(params)}`);
 }
