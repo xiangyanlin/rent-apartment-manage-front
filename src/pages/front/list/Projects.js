5d375ead36be1cb0478a5e4f70921088d4083f1e
xiangyanlin <2356150341@qq.com>
2020-04-06 14:11:35 +0800

    房源列表查询

--- src/pages/front/list/Projects.js
+++ src/pages/front/list/Projects.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Row, Col, Form, Card, Select, List, Pagination } from 'antd';
+import { Row, Col, Form, Card, Select, List, Pagination,Input,Button,Icon } from 'antd';
 import TagSelect from '@/components/TagSelect';
 import StandardFormRow from '@/components/StandardFormRow';
 import styles from './Projects.less';
@@ -28,6 +28,14 @@ const FormItem = Form.Item;
   },
 })
 class CoverCardList extends PureComponent {
+  state = {
+    modalVisible: false,
+    updateModalVisible: false,
+    expandForm: false,
+    selectedRows: [],
+    formValues: {},
+    stepFormValues: {},
+  };
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch({
@@ -61,6 +69,184 @@ class CoverCardList extends PureComponent {
     });
   };
 
+  handleSearch = e => {
+    e.preventDefault();
+
+    const { dispatch, form } = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+
+      const values = {
+        ...fieldsValue,
+        updatedAt: fieldsValue.updatedAt && fieldsValue.updatedAt.valueOf(),
+      };
+
+      this.setState({
+        formValues: values,
+      });
+
+      dispatch({
+        type: 'houseResource/fetch',
+        payload: values,
+      });
+    });
+  };
+
+  toggleForm = () => {
+    const { expandForm } = this.state;
+    this.setState({
+      expandForm: !expandForm,
+    });
+  };
+
+  renderSimpleForm() {
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 5, lg: 24, xl: 48 }}>
+          <Col md={4} sm={24}>
+            {getFieldDecorator('status')(
+              <Select placeholder="房屋状态" style={{ width: '100%' }}>
+                <Option value="1">待租</Option>
+                <Option value="2">租出</Option>
+              </Select>
+            )}
+          </Col>
+          <Col md={4} sm={24}>
+            {getFieldDecorator('title')(<Input placeholder="楼盘名称" />)}
+          </Col>
+          <Col md={4} sm={24}>
+            {getFieldDecorator('used')(
+              <Select placeholder="房屋类型" style={{ width: '100%' }}>
+                <Option value="1">住宅</Option>
+                <Option value="2">商住两用</Option>
+              </Select>
+            )}
+          </Col>
+          <Col md={4} sm={24}>
+            {getFieldDecorator('houseType')(<Input placeholder="户型" />)}
+          </Col>
+          <Col md={8} sm={24}>
+            <span className={styles.submitButtons}>
+              <Button type="primary" htmlType="submit">
+                查询
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+                重置
+              </Button>
+              <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+                展开 <Icon type="down" />
+              </a>
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderAdvancedForm() {
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 6, lg: 24, xl: 48 }}>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('status')(
+                <Select placeholder="房屋状态" style={{ width: '100%' }}>
+                  <Option value="0">待租</Option>
+                  <Option value="1">租出</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('title')(<Input placeholder="楼盘名称" />)}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('used')(
+                <Select placeholder="房屋类型" style={{ width: '100%' }}>
+                  <Option value="0">住宅</Option>
+                  <Option value="1">商住两用</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('houseType')(<Input placeholder="户型" />)}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row gutter={{ md: 6, lg: 24, xl: 48 }}>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('id')(<Input placeholder="房源编号" />)}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={24}>
+            <Row>
+              <Col md={10} sm={24}>
+                <FormItem label="">
+                  {getFieldDecorator('minRent')(<Input placeholder="价格" />)}
+                </FormItem>
+              </Col>
+              <Col md={4} sm={24}>
+                <div style={{ textAlign: 'center' }}>到</div>
+              </Col>
+              <Col md={10} sm={24}>
+                <FormItem label="">
+                  {getFieldDecorator('maxRent')(<Input placeholder="价格" />)}
+                </FormItem>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('orientation')(<Input placeholder="朝向" />)}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={24}>
+            <FormItem label="">
+              {/* {getFieldDecorator('name')(<Input placeholder="装修" />)} */}
+              {getFieldDecorator('decoration')(
+                <Select placeholder="装修" style={{ width: '100%' }}>
+                  <Option value="1">精装</Option>
+                  <Option value="2">简装</Option>
+                  <Option value="3">毛坯</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <div style={{ overflow: 'hidden' }}>
+          <div style={{ float: 'right', marginBottom: 24 }}>
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+              重置
+            </Button>
+            <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+              收起 <Icon type="up" />
+            </a>
+          </div>
+        </div>
+      </Form>
+    );
+  }
+
+  renderForm() {
+    const { expandForm } = this.state;
+    return expandForm ? this.renderAdvancedForm() : this.renderSimpleForm();
+  }
   render() {
     const { list, pagination } = this.props.houseResource.data;
     const { loading, form } = this.props;
@@ -129,51 +315,24 @@ class CoverCardList extends PureComponent {
     return (
       <div>
         <Card bordered={false}>
-          <Form layout="inline">
-            <StandardFormRow title="所属类目" block style={{ paddingBottom: 11 }}>
-              <FormItem>
-                {getFieldDecorator('category')(
-                  <TagSelect expandable>
-                    <TagSelect.Option value="cat1">类目一</TagSelect.Option>
-                    <TagSelect.Option value="cat2">类目二</TagSelect.Option>
-                    <TagSelect.Option value="cat3">类目三</TagSelect.Option>
-                    <TagSelect.Option value="cat4">类目四</TagSelect.Option>
-                    <TagSelect.Option value="cat5">类目五</TagSelect.Option>
-                    <TagSelect.Option value="cat6">类目六</TagSelect.Option>
-                    <TagSelect.Option value="cat7">类目七</TagSelect.Option>
-                    <TagSelect.Option value="cat8">类目八</TagSelect.Option>
-                    <TagSelect.Option value="cat9">类目九</TagSelect.Option>
-                    <TagSelect.Option value="cat10">类目十</TagSelect.Option>
-                    <TagSelect.Option value="cat11">类目十一</TagSelect.Option>
-                    <TagSelect.Option value="cat12">类目十二</TagSelect.Option>
-                  </TagSelect>
-                )}
-              </FormItem>
-            </StandardFormRow>
-            <StandardFormRow title="其它选项" grid last>
-              <Row gutter={16}>
-                <Col lg={8} md={10} sm={10} xs={24}>
-                  <FormItem {...formItemLayout} label="作者">
-                    {getFieldDecorator('author', {})(
-                      <Select placeholder="不限" style={{ maxWidth: 200, width: '100%' }}>
-                        <Option value="lisa">王昭君</Option>
-                      </Select>
-                    )}
-                  </FormItem>
-                </Col>
-                <Col lg={8} md={10} sm={10} xs={24}>
-                  <FormItem {...formItemLayout} label="好评度">
-                    {getFieldDecorator('rate', {})(
-                      <Select placeholder="不限" style={{ maxWidth: 200, width: '100%' }}>
-                        <Option value="good">优秀</Option>
-                        <Option value="normal">普通</Option>
-                      </Select>
-                    )}
-                  </FormItem>
-                </Col>
-              </Row>
-            </StandardFormRow>
+          <Form layout="inline" onSubmit={this.handleSearch}>
+          <FormItem>
+          {getFieldDecorator('keyWord')(
+            <div style={{ textAlign: 'center' }}>
+            <Input.Search
+              placeholder="请输入"
+              enterButton="搜索"
+              size="large"
+              onSearch={this.handleFormSubmit}
+              style={{ width: 522 }}
+            />
+          </div>
+            )}
+          </FormItem>
+
+
           </Form>
+          <div className={styles.tableListForm}>{this.renderForm()}</div>
         </Card>
         <div className={styles.cardList}>{cardList}</div>
         <div className={styles.pagination}>
