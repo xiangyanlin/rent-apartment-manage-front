f9d5c5a7cac681ce0293689ba35cc8cbda03b3c1
xiangyanlin <2356150341@qq.com>
2020-04-29 21:48:39 +0800

    问答详情

--- src/pages/front/Question/QuestionDetail.js
+++ src/pages/front/Question/QuestionDetail.js
@@ -1,11 +1,78 @@
 import React, { PureComponent } from 'react';
+import {
+  Comment,
+  Icon,
+  Tooltip,
+  Avatar,
+  Input,
+  Form,
+  Card,
+  Select,
+  List,
+  Button,
+  Row,
+  Col,
+  Tag,
+} from 'antd';
+import styles from './Question.less';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
 
-class Contract extends React.Component{
-    render(){
-        return (
-            <div>问答详情</div>
-        );
-    }
+class QuestionDetail extends PureComponent {
+  render() {
+    const { question } = this.props.location.state;
+    return (
+      <div className={styles.container}>
+        <Card>
+          <h1>{question.questions}</h1>
+          <p className={styles.quer}>
+            <span>
+              提问者：
+              {question.questioner}
+            </span>
+            &nbsp;
+            <span>
+              时间：
+              {moment(question.quiz_time).format('YYYY-MM-DD HH:mm:ss')}
+            </span>
+          </p>
+          {question.summary ? (
+            <div>
+              <Avatar
+                src={'http://127.0.0.1:8080/common/getImage?filename=' + question.questionerAvatar}
+                alt={question.questioner}
+              />
+              <p className={styles.info}>
+                <span />
+                {question.summary}
+              </p>
+            </div>
+          ) : (
+            <div />
+          )}
+        </Card>
+        <Card style={{ marginTop: '10px', minHeight: '300px' }}>
+          <Comment
+            // actions={actions}
+            author={question.answerer}
+            avatar={
+              <Avatar
+                src={'http://127.0.0.1:8080/common/getImage?filename=' + question.answererAvatar}
+                alt={question.answerer}
+              />
+            }
+            content={<p>{question.answer}</p>}
+            datetime={
+              <Tooltip title={moment(question.answer_time).format('YYYY-MM-DD HH:mm:ss')}>
+                <span>{moment(question.answer_time).fromNow()}</span>
+              </Tooltip>
+            }
+          />
+        </Card>
+      </div>
+    );
+  }
 }
 
-export default Contract;
+export default QuestionDetail;
